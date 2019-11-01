c85ab734f58fad8fed97cc5c488bbd3f65757e36
Sato,Nao <nsato@sonix.asia>
2019-11-01 12:11:30 +0900

    Todoモデルにバリデーションを追加

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,8 @@
 class Todo < ApplicationRecord
+    #validation
+    validates :title, presence: true
+    validates :detail, presence: true
+    VALID_DATE_REGEX = /^\d{4}-\d{1,2}-\d{5}:\d{2}:\d{2}.\d{4}$/
+    validates :date, presence: true, 
+                     format:{with: VALID_DATE_REGEX}
 end
