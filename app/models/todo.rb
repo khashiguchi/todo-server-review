10fbe9329db9c72f3e40209303014ed71a7ef1e5
Sato,Nao <nsato@sonix.asia>
2019-11-05 15:16:16 +0900

    定数を移動

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,7 @@
 class Todo < ApplicationRecord
+ MAX_TITLE_LIMIT = 100
+ MAX_DETAIL_LIMIT = 1000
 # validation
-  validates :title, presence: true, length: { maximum: 100 }
-　validates :detail, length: { maximum: 1000 }
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LIMIT }
+　validates :detail, length: { maximum: MAX_DETAIL_LIMIT }
 end　
--- db/migrate/20191101012851_create_todos.rb
+++ db/migrate/20191101012851_create_todos.rb
@@ -1,11 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
-  MAX_TITLE_LIMIT = 100
-  MAX_DETAIL_LIMIT = 1000
-
   def change
     create_table :todos do |t|
-      t.string :title, :null => false, :limit => MAX_TITLE_LIMIT
-      t.string :detail, :limit => MAX_DETAIL_LIMIT
+      t.string :title, :null => false, :limit => 100
+      t.string :detail, :limit => 1000
       t.datetime :date
 
       t.timestamps
