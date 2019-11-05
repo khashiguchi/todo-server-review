7c91735a5d5c1e2cf05a6eb784e12bdb70ebe98c
Sato,Nao <nsato@sonix.asia>
2019-11-05 17:13:49 +0900

    マイグレーションファイルの修正

--- db/migrate/20191101012851_create_todos.rb
+++ db/migrate/20191101012851_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
       t.string :title, null: false, limit: 100
-      t.string :detail, :limit: 1000
+      t.string :detail, limit: 1000
       t.datetime :date
 
       t.timestamps
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,44 +4,44 @@ RSpec.describe Todo, type: :model do
 
   before(:each) { @todo = create(:todo) }
 
-  describe "title" do
-    it "is valid" do
+  describe 'title' do
+    it 'is valid' do
       expect(@todo).to be_valid
     end
 
-    context "is not present," do
-      before { @todo.title= "" }
-      it "is invalid" do
+    context 'is not present,' do
+      before { @todo.title= '' }
+      it 'is invalid' do
         expect(@todo).to be_invalid
       end
     end
 
-    context "has 100 characters," do
-      before { @todo.title = "a" * 100 }
-      it "is valid" do
+    context 'has 100 characters,' do
+      before { @todo.title = 'a' * 100 }
+      it 'is valid' do
         expect(@todo).to be_valid
       end
     end
 
-    context "has 101 characters," do
-      before { @todo.title= "a" * 101 }
-      it "is invalid" do
+    context 'has 101 characters,' do
+      before { @todo.title= 'a' * 101 }
+      it 'is invalid' do
         expect(@todo).to be_invalid
       end
     end
   end
 
-  describe "detail" do
-    context "has 1000characters," do
-      before {@todo.detail="a"*1000}
-      it "is valid" do
+  describe 'detail' do
+    context 'has 1000characters,' do
+      before {@todo.detail='a'*1000}
+      it 'is valid' do
         expect(@todo).to be_valid
       end
     end
 
-    context "has 1001 characters" do
-      before{@todo.detail="a"*1001}
-      it "is invalid" do
+    context 'has 1001 characters' do
+      before{@todo.detail='a'*1001}
+      it 'is invalid' do
         expect(@todo).to be_invalid
       end
     end
