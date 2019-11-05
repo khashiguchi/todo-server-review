0b0149cb55c0ed6623ef74d3d2a378146046849f
Sato,Nao <nsato@sonix.asia>
2019-11-05 17:21:48 +0900

    {}の前後にスペースを追加

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Todo, type: :model do
     end
 
     context 'is not present,' do
-      before { @todo.title= '' }
+      before { @todo.title = '' }
       it 'is invalid' do
         expect(@todo).to be_invalid
       end
@@ -24,7 +24,7 @@ RSpec.describe Todo, type: :model do
     end
 
     context 'has 101 characters,' do
-      before { @todo.title= 'a' * 101 }
+      before { @todo.title = 'a' * 101 }
       it 'is invalid' do
         expect(@todo).to be_invalid
       end
@@ -33,14 +33,14 @@ RSpec.describe Todo, type: :model do
 
   describe 'detail' do
     context 'has 1000characters,' do
-      before {@todo.detail='a'*1000}
+      before { @todo.detail = 'a' * 1000 }
       it 'is valid' do
         expect(@todo).to be_valid
       end
     end
 
     context 'has 1001 characters' do
-      before{@todo.detail='a'*1001}
+      before{ @todo.detail = 'a' * 1001 }
       it 'is invalid' do
         expect(@todo).to be_invalid
       end
